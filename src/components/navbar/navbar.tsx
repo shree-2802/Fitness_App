72e349fec2e7b8359884c0b0a8c4da57ad6163a8
shree-2802 <pattupadmashree@gmail.com>
2024-01-28 23:09:41 +0530

    tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,40 @@
+import { logo } from '../../assets';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaSquareXmark } from 'react-icons/fa6';
 import './navbar.scss';
+import { useState } from 'react';
 
-const Navbar = () => {
+const NavLinks = () => {
   return (
-    <div>N</div>
-  )
-}
+    <div className='fitness__navbar-links'>
+      <a href='#home'>Home</a>
+      <a href='#exercise'>Exercise</a>
+    </div>
+  );
+};
+const Navbar = () => {
+  const [hamburger, setHamburger] = useState(false);
+  return (
+    <div className='fitness__navbar flex_both-space-between'>
+      <img src={logo} alt='logo' />
+      <div>
+        {hamburger ? (
+          <FaSquareXmark
+            fontSize={27}
+            onClick={() => setHamburger(!hamburger)}
+          />
+        ) : (
+          <GiHamburgerMenu
+            fontSize={27}
+            onClick={() => setHamburger(!hamburger)}
+          />
+        )}
 
-export default Navbar
+        {hamburger && <NavLinks />}
+      </div>
+      <NavLinks />
+    </div>
+  );
+};
+
+export default Navbar;
